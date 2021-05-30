464f0f6e4c97f017c630426d17d1fa62c1d537f8
Facundo Muñoz <facundo.munoz@cirad.fr>
2021-05-30 22:48:21 +0200

    Application launcher: vibass_app().

---
+++ R/vibass_app.R
@@ -0,0 +1,23 @@
+#' VIBASS interactive apps.
+#'
+#' Launches the Shiny interactive applications for the practicals.
+#'
+#' @param x integer 1. Practical id.
+#'
+#' @import shiny extraDistr colorspace ggplot2 dplyr tibble magrittr tidyr
+#' @export
+vibass_app <- function (x = 1, bg = FALSE) {
+
+  path <- paste0("\"apps/p", x, "\"")
+  code_launch <- paste0(
+    "shiny::runApp(system.file(",
+    path, ", package = \"vibass\"), launch.browser = TRUE)"
+  )
+
+  if (bg) {
+    system(paste("Rscript -e '", code_launch, "' &"))
+  } else {
+    eval(parse(text = code_launch))
+  }
+}
+
