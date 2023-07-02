88bd78ff52d5f097771cdf5a191c2805070012f0
Virgilio Gómez-Rubio <virgilio.gomez@uclm.es>
2023-07-02 11:08:42 +0200

    Documentation for Weights dataset.

---
+++ R/Weights.R
@@ -0,0 +1,24 @@
+#' Weights of children
+#'
+#' Data for the VIBASS session on linear models. The dataset include
+#' data bout children, that includes weight, height and some other variables.
+#'
+#' @docType data
+#'
+#' @usage data(Weights)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @references To be added.
+#'
+#' @source To be added.
+#'
+#' @examples
+#' data(Weights)
+#' summary(Weights)
+#' 
+#' # ML estimates
+#' lmW <- lm(weight ~ age, data = Weights)
+#' summary(lmW) 
